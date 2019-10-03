e192075048a2be37712dee81e79d5e9cd22268f6
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-10-03 09:56:38 +0530

    Version 1.0

--- routes.js
+++ routes.js
@@ -1,15 +1,3 @@
-const city = [
-  {  name: "Maharashtra", city: "Pune"},
-  {  name: "Maharashtra", city: "Mumbai"},
-  {  name: "Karnataka", city: "Bangalore"},
-  {  name: "Maharashtra", city: "Kolhapur"},
-  {  name: "Maharashtra", city: "Satara"},
-  {  name: "Maharashtra", city: "Karad"},
-  {  name: "Maharashtra", city: "Sangali"},
-  {  name: "Tamilnadu", city: "Chennai"},
-  {  name: "Goa", city: "Panaji"}
-
-];
 let tempc;
 var d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
 var newD = new Date();
@@ -20,43 +8,79 @@ document.getElementById("time").innerHTML =week[n]+" , "+d;
 function process()
 {
       var selectedValue=document.getElementById("city").value;
-      var request = new XMLHttpRequest()
+      //console.log(selectedValue);
+       var request = new XMLHttpRequest()
       // Open a new connection, using the GET request on the URL endpoint
-      request.open('GET', 'http://api.openweathermap.org/data/2.5/find?q='+selectedValue+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
-      request.onload = function() {
+       request.open('GET', 'http://api.openweathermap.org/data/2.5/find?q='+selectedValue+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
+       request.onload = function() {
         // Begin accessing JSON data here
-      var data = JSON.parse(this.response);
-      console.log(data);
-      console.log(data.list[0].weather[0].icon);
-     // tempc=data.list[0].main.temp;
-      var temp=city.filter((v)=> v.city===selectedValue);
-      document.getElementById("state").innerHTML=temp[0].city+" , "+temp[0].name;
-      document.getElementById("temp").innerHTML=data.list[0].main.temp;
-      document.getElementById("condition").innerHTML=data.list[0].weather[0].description;
-      document.getElementById("button1").style.color = "black";
-      document.getElementById("button2").style.color = "#666666";
+       var data = JSON.parse(this.response)
+       console.log(data);
+       //console.log(data.list[0].name)
+       document.getElementById("state").innerHTML=data.list[0].name;
+       document.getElementById("temp").innerHTML=data.list[0].main.temp;
+       document.getElementById("condition").innerHTML=data.list[0].weather[0].description;
+       document.getElementById("button1").style.color = "black";
+       document.getElementById("button2").style.color = "#666666";
+       let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
+       $("#icon").attr("src",icon);
+       document.getElementById("button1").style.visibility = "visible";
+       document.getElementById("button2").style.visibility = "visible";
       }
      request.send();
 }
-document.getElementById("button2").style.color = "#666666";
-function findTemp()
-{
-  var selectedValue=document.getElementById("city").value;
-  var request = new XMLHttpRequest()
-  // Open a new connection, using the GET request on the URL endpoint
-     request.open('GET', 'http://api.openweathermap.org/data/2.5/find?q='+selectedValue+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
+
+ function findTemp()
+ {
+   var selectedValue=document.getElementById("city").value;
+   var request = new XMLHttpRequest()
+   // Open a new connection, using the GET request on the URL endpoint
+      request.open('GET', 'http://api.openweathermap.org/data/2.5/find?q='+selectedValue+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
       request.onload = function() {
-    // Begin accessing JSON data here
-       var data = JSON.parse(this.response);
-       var temp=city.filter((v)=> v.city===selectedValue);
-      document.getElementById("state").innerHTML=temp[0].city+" , "+temp[0].name;
+             // Begin accessing JSON data here
+      var data = JSON.parse(this.response);
+      document.getElementById("state").innerHTML=data.list[0].name;
       let NewData=Math.round((data.list[0].main.temp*1.8)+32);
       document.getElementById("temp").innerHTML=NewData;
       document.getElementById("condition").innerHTML=data.list[0].weather[0].description;
       document.getElementById("button2").style.color = "black";
       document.getElementById("button1").style.color = "#666666";
+      document.getElementById("button1").style.visibility = "visible";
+      document.getElementById("button2").style.visibility = "visible";
+      let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
+      $("#icon").attr("src",icon);
   }
   request.send()
 }
 
 
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+    
+  }
+}
+
+function showPosition(position) {
+  var request = new XMLHttpRequest()
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
+     request.onload = function() {
+    // Begin accessing JSON data here
+     var data = JSON.parse(this.response);
+      document.getElementById("state").innerHTML=data.name;
+      document.getElementById("temp").innerHTML=data.main.temp;
+      document.getElementById("condition").innerHTML=data.weather[0].description;
+     document.getElementById("button2").style.color = "black";
+     document.getElementById("button1").style.color = "#666666";
+     document.getElementById("button1").style.visibility = "visible";
+     document.getElementById("button2").style.visibility = "visible";
+     let icon="http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+     $("#icon").attr("src",icon);
+   console.log(data);
+ }
+ request.send()
+}
