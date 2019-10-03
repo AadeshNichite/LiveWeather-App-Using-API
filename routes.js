bf2ca4549409a191db59d9011f7185fa0fe2522d
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-10-03 21:21:09 +0530

    Version 1.0

--- routes.js
+++ routes.js
@@ -8,7 +8,11 @@ document.getElementById("time").innerHTML =week[n]+" , "+d;
 function process()
 {
       var selectedValue=document.getElementById("city").value;
-      //console.log(selectedValue);
+      console.log(selectedValue);
+      if(selectedValue == "")
+      {
+       selectedValue=document.getElementById("state").textContent;
+      }
        var request = new XMLHttpRequest()
       // Open a new connection, using the GET request on the URL endpoint
        request.open('GET', 'http://api.openweathermap.org/data/2.5/find?q='+selectedValue+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
@@ -31,37 +35,18 @@ function process()
 }
 
  function findTemp()
- {
-   var selectedValue=document.getElementById("city").value;
-   //console.log(selectedValue);
-   var request = new XMLHttpRequest()
-   // Open a new connection, using the GET request on the URL endpoint
-      request.open('GET', 'http://api.openweathermap.org/data/2.5/find?q='+selectedValue+'&units=metric&appid=af8a888b5c0d1f1ce6a286db966af8ed', true)
-      request.onload = function() {
-             // Begin accessing JSON data here
-      var data = JSON.parse(this.response);
-      document.getElementById("state").innerHTML=data.list[0].name;
-      let NewData=Math.round((data.list[0].main.temp*1.8)+32);
-      document.getElementById("temp").innerHTML=NewData;
-      document.getElementById("condition").innerHTML=data.list[0].weather[0].description;
-      document.getElementById("button2").style.color = "black";
-      document.getElementById("button1").style.color = "#666666";
-      document.getElementById("button1").style.visibility = "visible";
-      document.getElementById("button2").style.visibility = "visible";
-      let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
-      $("#icon").attr("src",icon);
-  }
-  request.send()
+ { 
+   let tempc=document.getElementById("temp").textContent;
+   console.log(tempc);
+   let NewData=Math.round((tempc*1.8)+32);
+  document.getElementById("temp").innerHTML=NewData;
+
 }
 function onloadData()
 {
   alert("Allow Location?");
   getLocation();
-  showPosition(position);
 }
-
-
-
   function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
